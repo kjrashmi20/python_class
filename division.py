0e9ae3cbdf62b735dd462acaeab5d815b4316024
Rashmi <rashmikj1992@gmail.com>
2018-10-13 18:15:57 +0530

    Find max

--- division.py
+++ division.py
@@ -3,10 +3,10 @@
 num1=int(input("enter number 1"))
 num2=int(input("enter number 2"))
 num3=num1%num2
-if num3==0:
-    print("num1 is divisible by num2".format(num1,num2))
+if (num3==0):
+    print("{} is divisible by {}".format(num1,num2))
 else:
-    print("num1 is not divisible by num2".format(num1,num2))
+    print("{} is not divisible by {}".format(num1,num2))
 
 
 
---
+++ maximum.py
@@ -0,0 +1,6 @@
+a=int(input("enter num1"))
+b=int(input("enter num2"))
+if(a>b):
+    print("Max number is {}".format(a))
+else:
+    print("Max number is {}".format(b))
